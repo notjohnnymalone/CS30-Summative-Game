76a8bab5bc8eedc486f317f44ac3910706e6f08e
SirRipher <90797166+SirRipher@users.noreply.github.com>
2021-12-09 13:32:51 -0600

    Add files via upload

--- CompSci_30_Final_Game_Graden_Reid_Dec_6_2021.py
+++ CompSci_30_Final_Game_Graden_Reid_Dec_6_2021.py
@@ -18,18 +18,57 @@ WIDTH = 480#we can change this later.
 HEIGHT = 600
 FPS = 60
 
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
 #Get Pygame all set to use.
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Regular Computer Classmates 2: Return Of Browser")
 clock = pygame.time.Clock()
+sprites = pygame.sprite.Group()
 
 #player Class
-class Player(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):#stes attributes for the 2nd player
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(player_img, (50, 38))
-        self.image = 
+        self.image = pygame.Surface((16, 60))
+        self.image.fill(WHITE)
         self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT / 2
+
+    def update(self):
+        self.speedy = 0
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_SPACE]:
+            self.speedy = -8
+        if keystate[pygame.K_a]:
+            self.speedx = -8
+        if keystate[pygame.K_d]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
         
+player = Player()
+sprites.add(player)
+running = True
+game_over = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #screen.blit(background, background_rect)
+    sprites.update()
+    screen.fill(RED)
+    sprites.draw(screen)
+    pygame.display.flip()
+pygame.quit()
