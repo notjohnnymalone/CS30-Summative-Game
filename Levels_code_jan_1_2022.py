97183cccb43c5332ea7b1fbc282d888d446b6a38
SirRipher <90797166+SirRipher@users.noreply.github.com>
2022-01-01 21:41:33 -0600

    Levels Code
    Add this levels code into your saved files.

---
+++ Levels_code_jan_1_2022.py
@@ -0,0 +1,78 @@
+##########
+#Pygame_Final_Project
+#Mobs_Code_dec_31_2021
+#Graden_Rusk_and_Reid_Martin
+#Dec_3_2021-Jan_2022
+##########
+
+level = [
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+        "P                              P",
+        "P                              P",
+        "P                              P",
+        "P                              P",
+        "P P       P                    P",
+        "P PPPPPPPPP                    P",
+        "P            PPP               P",
+        "P                              P",
+        "P    PPPPPP          P         P",
+        "P    P               PPPPPPPPPPP",
+        "P    P                         P",
+        "P    P K    P                  P",
+        "P    PPPPPPPP    PP            P",
+        "P                            L P",
+        "P                          PPPPP",
+        "P                 PPPPPP       P",
+        "P                              P",
+        "PPPPP     PPPPPPP        PPPPPPP",
+        "P   P                      P   P",
+        "P   P                      P   P",
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",]
+
+level_2 = [
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+        "P      P                       P",
+        "P      P                       P",
+        "PPP PPPPPPPP  PPPPPPPPPPP   PPPP",
+        "P              P        P   P  P",
+        "P              P        P   P  P",
+        "PPPPPPPPPPPPPPPPPPPPPPPPP   P  P",
+        "P    P              P       PPPPP",
+        "P    P              P          P",
+        "P    PPP PPPPPPPPP PP  P       P",
+        "P      P P  P       P  PPPPPPPPP",
+        "P   PPPP PPPP       PP     P   P",
+        "P   P       P     P  P     P   P",
+        "P   PPPP PPPP        PPPP  P   P",
+        "P      P P  P          PPP P   P",
+        "P           PPPP PPPPPPPPP PPPPP",
+        "P              P               P",
+        "P              P               P",
+        "PPPPP          PPPPPPPPPPPPPPPPP",
+        "P   P                          P",
+        "P   P        P                 P",
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",]
+
+level_3 = [
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+        "P                              P",
+        "P                              P",
+        "PPPPPP PPPP                    P",
+        "P   PP PP                      P",
+        "P   P   P                      P",
+        "P   PP PP      PPPPPPPPPPP PPPPP",
+        "P    P  P      P         P P   P",
+        "P    P  P    PPP        PP PP  P",
+        "P    PP P      P        P   P  P",
+        "P     P P      P        PP PP  P",
+        "P    PP PPP    P         P P   P",
+        "P    P   P     P         P P   P",
+        "P    PP PP     P         P P   P",
+        "P     P PPPPP PP         P P   P",
+        "P     P        P         P P   P",
+        "P     P        P         P P   P",
+        "P     PPPPPPP PP         P P   P",
+        "PPPPPPPPPPPPP PPPPPPP PPPP PPPPP",
+        "P                          P   P",
+        "P                          P   P",
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",]
